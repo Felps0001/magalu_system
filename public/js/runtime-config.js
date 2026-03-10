89d6faf7a497a610169c950e075d0dd1ffe50be3
Fellipe Fontes <fellipe.fontes@grifa.digital>
2026-03-10 20:20:01 -0300

    ajustes gerais

--- public/js/runtime-config.js
+++ public/js/runtime-config.js
@@ -1,5 +1,5 @@
 window.MAGALU_RUNTIME_CONFIG = window.MAGALU_RUNTIME_CONFIG || {
   // Para GitHub Pages, informe aqui a URL publica do backend Node/Express.
   // Exemplo: apiBaseUrl: 'https://seu-backend.onrender.com'
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://magalu-system.onrender.com',
 };
--- src/app.js
+++ src/app.js
@@ -9,6 +9,18 @@ function createApp() {
   const publicDirectory = path.join(__dirname, '..', 'public');
 
   app.disable('x-powered-by');
+  app.use((request, response, next) => {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, ngrok-skip-browser-warning');
+
+    if (request.method === 'OPTIONS') {
+      response.sendStatus(204);
+      return;
+    }
+
+    next();
+  });
   app.use(express.json());
   app.use(express.static(publicDirectory));
   app.use(webRoutes);
