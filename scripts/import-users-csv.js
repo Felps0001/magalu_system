62bbfb26d1f5ebfc98fe80de017b1db0ab414589
LuisFontes <fellipe_fontes@outlook.com>
2026-03-25 14:33:16 -0300

    ajuste agenda

--- scripts/import-users-csv.js
+++ scripts/import-users-csv.js
@@ -7,7 +7,7 @@ const { ObjectId } = require('mongodb');
 const { connectToMongoDB, closeMongoDBConnection } = require('../src/config/mongodb');
 const { createUser, createUserQrPayload } = require('../src/models/user');
 
-const DEFAULT_CSV_FILE = path.resolve(__dirname, '..', 'ROSA-MTG.csv');
+const DEFAULT_CSV_FILE = path.resolve(__dirname, '..', 'Participantes-aiqfome-PE26.csv');
 
 function parseArgs(argv) {
   const options = {
